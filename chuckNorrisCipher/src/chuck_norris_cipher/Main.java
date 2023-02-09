2b94c60a06a54f437d1c3bcfcba00e5091b78b81
Krig Raseri <krigraseri@gmail.com>
2023-02-09 06:03:44 -0600

    Update Main.java
    Starts the project off with taking a string and then splitting it up. Hello would be H e l l o

--- chuckNorrisCipher/src/chuck_norris_cipher/Main.java
+++ chuckNorrisCipher/src/chuck_norris_cipher/Main.java
@@ -1,7 +1,20 @@
 package chuck_norris_cipher;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //The biggest buffest reader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input string:");
+        String[] input = reader.readLine().split("");
+
+
+        for (String x : input) {
+            System.out.print(x + " ");
+        }
 
     }
 }
